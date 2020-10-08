ce1ba7f83cab6846b249064a0c6cc3e5f3bc7503
jgmtzz <jgmtzz17@gmail.com>
2020-10-07 21:36:30 -0500

    prueba

--- src/main/java/archy/soft/quizter/model/Preguntas.java
+++ src/main/java/archy/soft/quizter/model/Preguntas.java
@@ -1,5 +1,30 @@
 package archy.soft.quizter.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Preguntas {
 
+	@Id 
+	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column
+	private String pregunta;
+	@Column
+    private int tipo_respuesta;
+	@Column
+	private char status;
+	@Column
+	private String fecha_actualizacion;  
+	@Column
+	private int tema_id; 
+	@Column
+	private int users_id;       
+	
+	
 }
